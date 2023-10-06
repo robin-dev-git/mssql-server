89cf5c8ce9e3831571484f7d4ffbf9b897741d10
robin-dev-git <robin-0507@outlook.com>
2023-10-06 14:48:52 -0500

    modifican los archivos que arreglo

--- install-mssql-server-2019.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install -y curl wget git \
-  curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc \
-  sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)" \
-  sudo apt-get update \
-  sudo apt-get install -y mssql-server \
-  sudo /opt/mssql/bin/mssql-conf setup \
-  systemctl status mssql-server --no-pager
